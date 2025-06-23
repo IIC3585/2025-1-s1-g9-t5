fcf8073b5cda33b7888640fbde7df269ade9c94b
Stomicicp <stomicicp@uc.cl>
2025-06-23 09:56:28 -0400

    new deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [vue(), react(), tailwind()],
+  site: 'https://iic3585.github.io/2025-1-s1-g9-t5',
+  base: '/2025-1-s1-g9-t5/',
   output: 'static',
-  site: 'https://moviefan.com',
   vite: {
     css: {
       preprocessorOptions: {
